5dbc7ee8606666a0a309250bca9d95a6ab739f8d
lukhand <lukhand@MININT-3SJKT88>
2016-12-06 17:54:28 -0500

    Added some inline comments..

--- Client/TestSSHClient/TestSSHClient/Program.cs
+++ Client/TestSSHClient/TestSSHClient/Program.cs
@@ -13,15 +13,18 @@ namespace TestSSHClient
     {
         static void Main(string[] args)
         {
-           
+           //Read parameter value from appconfig.
             string iIP = ConfigurationManager.AppSettings["IP"];
             string iUser = ConfigurationManager.AppSettings["User"];
             string iPassword = ConfigurationManager.AppSettings["Password"];
             string ifilename = "test";
             string storageaccount = ConfigurationManager.AppSettings["storageaccount"];
             string storagekey = ConfigurationManager.AppSettings["storagekey"];
-
+            //create a guid which will be used as the processid
             Guid g = Guid.NewGuid();
+
+            //buiding parameter value for the custom script.
+            //Note calling activate.sh
             string activatecmd = string.Format(
                 "/home/lkmsft/connector/activate.sh {0} {1} '{2}' '{3}' '{4}' '{5}' ",
                 "/home/lkmsft/connector/testffmpeg/testffmpeg.sh", 
@@ -30,20 +33,26 @@ namespace TestSSHClient
                 ifilename,
                 storageaccount,
                 storagekey);
+
             SshCommand rt;
 
             Console.WriteLine("process id" + g.ToString());
+            //connect to the VM
             using (var client = new SshClient(iIP, iUser, iPassword))
         {
             client.Connect();
+            
+            //run command activate.sh 
             rt = client.RunCommand(activatecmd);
                 Console.WriteLine(rt);
 
+            //now check status buy calling wait.sh.  
+            //keep checking while return status is 0 means still processing.
+            //stop if return code is 1 (finished with error) or 2 (finished with success). 
                    do
                     {
                        rt =  client.RunCommand("/home/lkmsft/connector/wait.sh " + g);
-                    //Console.WriteLine(x);
-
+                   
                    
                     Console.WriteLine(rt.ExitStatus);
                     Console.WriteLine(rt.Result);
